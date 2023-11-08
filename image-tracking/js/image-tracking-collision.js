168e43ec5eacfa23f5c93490368c3aa68ec5288b
Squareys <squareys@googlemail.com>
2023-11-08 10:03:56 +0000

    js: No need for customization anymore.
    Signed-off-by: Squareys <squareys@googlemail.com>

--- image-tracking/js/image-tracking-collision.js
+++ image-tracking/js/image-tracking-collision.js
@@ -1,10 +1,11 @@
 import {ImageTracking} from "@wonderlandengine/mind-ar-tracking";
+import { mat4,vec3 } from "gl-matrix";
 
 /**
  * image-tracking-coliision
  */
 export class ImageTrackingCollision extends ImageTracking {
-	static TypeName = "image-tracking-collision";
+	static TypeName = "image-tracking";
 	/* Properties that are configurable in the editor */
 	static InheritProperties = true;
 }
